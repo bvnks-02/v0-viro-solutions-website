0fdd1b64950355e10aa692cffcd24d02c88a5f30
bvnks-02 <n.benkrouidem@esi-sba.dz>
2026-01-07 09:23:30 -0600

    Configure serverExternalPackages for Nodemailer
    Added serverExternalPackages to prevent issues with Nodemailer.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Add this section to stop Next.js from breaking Nodemailer
+  serverExternalPackages: ["nodemailer"], 
+  
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
 }
 
 export default nextConfig
